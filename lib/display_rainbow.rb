a901061a75e0cb8e3892df735b31e669058a4214
Halyette <haly.lesane@gmail.com>
2018-03-12 23:50:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,9 @@
 def display_rainbow(colors)
   puts "(R: red)"
-  puts "(O: orange)", Y: yellow, G: green, B: blue, I: indigo, V: violet)"
+  puts "(O: orange)"
+  puts "(Y: yellow)" 
+  puts "(G: green)"
+  puts "(B: blue)"
+  puts "(I: indigo)"
+  puts "(V: violet)"
 end
